200943211474fe32eec4393579900cf9f6931e13
Ashwani K <ashwanikuntal@gmail.com>
2021-12-23 00:06:04 +0530

    Adding initial files

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = "AKIARG23SQI4MEU4CF6M"
+}
+variable "aws_secret_key" {
+default = "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"
+ }
+variable "region" {
+    default = "ap-south-1"
+}
