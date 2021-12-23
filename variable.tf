2b251b9e37ef78454081db69e9f2f937bb7aa025
Ashwani <ashwanikuntal@users.noreply.github.com>
2021-12-23 10:36:47 +0530

    removing cre

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" {
-default = "AKIARG23SQI4MEU4CF6M"
+default = ""
 }
 variable "aws_secret_key" {
-default = "7a2je9MVjPOUS+p/wUvusTGT8AbD+9TViKcnHbNE"
+default = ""
  }
 variable "region" {
     default = "ap-south-1"
