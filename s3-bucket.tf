ad42fb1e2536ee9b4fa48053bbb75bd0574f58ed
Ashwani K <ashwanikuntal@gmail.com>
2022-01-02 02:02:32 +0530

    Adding initial files

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,14 @@
+locals {
+  bucket_exists = "true"
+}
+
+data "aws_bucket" "test" {
+  lb_exists = "${local.bucket_exists}"
+  depends_on    = ["aws_s3_bucket.onebucket"]
+}
+
 resource "aws_s3_bucket" "onebucket" {
+   count = local.bucket_exists == "true" ? 1 : 0
    bucket = "akuntal-bucket-terraform"
    acl = "private"
    versioning {
